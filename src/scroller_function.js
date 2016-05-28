03f69dd960433d717ffae825ba733e2d631811f4
mznco <xterrain15@gmail.com>
2016-05-28 13:09:07 -0400

    now able to read review text from files. Not yet able to display said text

--- src/scroller_function.js
+++ src/scroller_function.js
@@ -19,3 +19,16 @@ loop();
 function simple_loop() {
     document.getElementById('textslide').firstElementChild.innerHTML = "Hello, World!";
 }
+
+function LoadFile() {
+    var oFrame = document.getElementById("frmFile");
+    var strRawContents = oFrame.contentWindow.document.body.childNodes[0].innerHTML;
+    while (strRawContents.indexOf("\r") >= 0)
+        strRawContents = strRawContents.replace("\r", "");
+    var arrLines = strRawContents.split("\n");
+    alert("File " + oFrame.src + " has " + arrLines.length + " lines");
+    for (var i = 0; i < arrLines.length; i++) {
+        var curLine = arrLines[i];
+        alert("Line #" + (i + 1) + " is: '" + curLine + "'");
+    }
+}
